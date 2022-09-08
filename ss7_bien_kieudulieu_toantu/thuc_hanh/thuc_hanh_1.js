65fc23d6e78f7d83412ab2ad22cb3048401223c0
TranVanSy2005 <tranvansy197@gmail.com>
2022-09-08 17:21:07 +0700

    nopbaitapbai7

--- ss7_bien_kieudulieu_toantu/thuc_hanh/thuc_hanh_1.js
+++ ss7_bien_kieudulieu_toantu/thuc_hanh/thuc_hanh_1.js
@@ -5,8 +5,8 @@ let a=parseInt(inputa);
 let b=parseInt(inputb);
 let c=a%b;
 if(c === 0){
-    alert("a là bôi số của b")
+    alert("a là bội số của b")
 }
 else {
-    alert("a không là bôi số của b")
+    alert("a không là bội số của b")
 }
